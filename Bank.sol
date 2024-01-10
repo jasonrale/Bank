a862d26397209f754be34cafd677e13236e82674
Jason <2045330878@qq.com>
2024-01-10 19:48:17 +0800

    Fix

--- Bank.sol
+++ Bank.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract Bank {
     address public admin;
-    address[] private leadboard;
+    address[3] private leadboard;
     mapping(address => uint256) private balance;
 
     modifier onlyAdmin {
@@ -19,7 +19,7 @@ contract Bank {
         return balance[depositor];
     }
     
-    function displayLeadboard() external view returns(address[] memory) {
+    function displayLeadboard() external view returns(address[3] memory) {
         return leadboard;
     }
     
@@ -33,19 +33,21 @@ contract Bank {
         uint256 amount = msg.value;
         address msgSender = msg.sender;
         require(amount > 0, "Incorrect Value");
-        balance[msg.sender] = amount;
+        balance[msg.sender] += amount;
 
-        uint256 min = amount;
+        uint256 min = balance[msg.sender];
         uint8 modifyIndex = 3;
-        if(leadboard.length < 3) {
-            leadboard.push(msgSender);
-        } else {
-            for (uint8 i = 0; i < 3; i++) {
-                if (balance[leadboard[i]] < min ) {
-                    min = balance[leadboard[i]];
-                    modifyIndex = i;
-                }
+        for (uint8 i = 0; i < 3; i++) {
+            if (leadboard[i] == msgSender) {
+                modifyIndex = 3;
+                break;
             }
+            if (balance[leadboard[i]] < min) {
+                min = balance[leadboard[i]];
+                modifyIndex = i;
+            }
+        }
+        if (modifyIndex < 3) {
             leadboard[modifyIndex] = msgSender;
         }
     }
